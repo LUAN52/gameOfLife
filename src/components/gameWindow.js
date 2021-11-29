852f5c440b84252153690c7885626c719bade62d
Luan <luansouza600@gmail.com>
2021-11-29 20:51:14 -0300

    alteraçao no metodo de verificar vizinhos

--- src/components/gameWindow.js
+++ src/components/gameWindow.js
@@ -3,123 +3,108 @@ import React from "react";
 
 
 export default class GameWindow extends React.Component {
-  
+
 
     constructor(props) {
-     
+
         super(props)
 
         this.state = {
 
             grid: this.generateGrid(),
             auxiliaryGrid: this.generateGrid(),
-            buttom:false,
-            label:"iniciar jogo"
+            buttom: false,
+            label: "iniciar jogo"
         }
     }
-    
-    checknNeighbors(grid,i,j,lineSize,columnSize)
-    {   
-            
-          let countNeighbors = 0
-          let matrizAuxiliar;
 
-          if(this.state.auxiliaryGrid.length>0)
-          {
-              this.setState({auxiliaryGrid:this.generateGrid()})
-          }
+    checknNeighbors(grid, i, j, lineSize, columnSize) {
 
-          matrizAuxiliar = this.state.auxiliaryGrid;
-         
-        
-           if((i+1<lineSize)&&(grid[i+1][j]))
-           {
+        let countNeighbors = 0
+        let auxiliary;
+
+        if (this.state.auxiliaryGrid.length > 0) {
+            this.setState({ auxiliaryGrid: this.generateGrid() })
+        }
+
+        auxiliary = this.state.auxiliaryGrid;
+
+
+        if ((i + 1 < lineSize) && (grid[i + 1][j])) {
             countNeighbors++;
-            
-           }
-        
-           if((i-1>=0)&&(grid[i-1][j]) )
-           {
+
+        }
+
+        if ((i - 1 >= 0) && (grid[i - 1][j])) {
             console.log("aqui")
             countNeighbors++;
-           }
-        
-           if((j+1<columnSize)&&(grid[i][j+1]) )
-           {
-             countNeighbors++;
-           }
-        
-           if(((j-1>=0)) && grid[i][j-1])
-           {
+        }
+
+        if ((j + 1 < columnSize) && (grid[i][j + 1])) {
             countNeighbors++;
-           }
-        
-           if( (i+1<lineSize) &&(j+1<columnSize)&&(grid[i+1][j+1])  )
-           {
+        }
+
+        if (((j - 1 >= 0)) && grid[i][j - 1]) {
             countNeighbors++;
-           }
-        
-           if((i-1>=0) &&(j+1<columnSize)&&(grid[i-1][j+1]) )
-           {
+        }
+
+        if ((i + 1 < lineSize) && (j + 1 < columnSize) && (grid[i + 1][j + 1])) {
             countNeighbors++;
-           }
-        
-           if( (i+1<lineSize) &&(j-1>=0)&&(grid[i+1][j-1]) )
-           {
+        }
+
+        if ((i - 1 >= 0) && (j + 1 < columnSize) && (grid[i - 1][j + 1])) {
             countNeighbors++;
-           }
-        
-           
-           if((i-1>=0) &&(j-1>=0)&&(grid[i-1][j-1]) )
-           {
+        }
+
+        if ((i + 1 < lineSize) && (j - 1 >= 0) && (grid[i + 1][j - 1])) {
             countNeighbors++;
-           }
-        
-           
-          if(grid[i][j])
-          {
-            if((countNeighbors>3) || (countNeighbors<2))
-            {
-              
-              matrizAuxiliar[i][j]=false;
-              this.setState({auxiliaryGrid:matrizAuxiliar});
-              
+        }
+
+
+        if ((i - 1 >= 0) && (j - 1 >= 0) && (grid[i - 1][j - 1])) {
+            countNeighbors++;
+        }
+
+
+        if (grid[i][j]) {
+            if ((countNeighbors > 3) || (countNeighbors < 2)) {
+
+                auxiliary[i][j] = false;
+                this.setState({ auxiliaryGrid: matrizAuxiliar });
+
             }
-            else
-            {
-                matrizAuxiliar[i][j]=true;
-                this.setState({auxiliaryGrid:matrizAuxiliar});
+            else {
+                auxiliary[i][j] = true;
+                this.setState({ auxiliaryGrid: matrizAuxiliar });
             }
-          }
-            
-           
-           if((grid[i][j]===false) && (countNeighbors===3))
-           {
-                    console.log(`${i}--${j}`)
-                    matrizAuxiliar[i][j]=true;
-                    this.setState({auxiliaryGrid:matrizAuxiliar});
-                     
-           }
-       
+        }
+
+
+        if ((grid[i][j] === false) && (countNeighbors === 3)) {
+            console.log(`${i}--${j}`)
+            auxiliary[i][j] = true;
+            this.setState({ auxiliaryGrid: matrizAuxiliar });
+
+        }
+
     }
 
-    run(grid)
-    {   
-        grid.map((line,lineIndex)=>{
-           return line.map((column,columnIndex)=>{
-               return this.checknNeighbors(grid,lineIndex,columnIndex,50,50)
+    run(grid) {
+        grid.map((line, lineIndex) => {
+            return line.map((column, columnIndex) => {
+                return this.checknNeighbors(grid, lineIndex, columnIndex, 50, 50)
             })
         })
     }
 
-  
+
     generateGrid() {
-        
-        let celulas=[]
+
+        let cells = []
         for (let i = 0; i < 50; i++) {
-            celulas[i]=[]
+            cells[i] = []
             for (let j = 0; j < 50; j++) {
-                celulas[i][j] = false;
+                cells[i][j] = false;
 
             }
         }
@@ -127,64 +112,63 @@ export default class GameWindow extends React.Component {
     }
 
 
-    setButtom(estateButtom)
-    {
-        this.setState({buttom:estateButtom})
+    setButtom(estateButtom) {
+        this.setState({ buttom: estateButtom })
     }
 
-    setLabel(text)
-    {
-        this.setState({label:text})
+    setLabel(text) {
+        this.setState({ label: text })
     }
 
-    deleteStates(text)
-    {
-        this.setState({label:text})
-        this.setState({grid:this.generateGrid()})
-        this.setState({auxiliaryGrid:this.generateGrid()})
+    deleteStates(text) {
+        this.setState({ label: text })
+        this.setState({ grid: this.generateGrid() })
+        this.setState({ auxiliaryGrid: this.generateGrid() })
     }
 
     render() {
-       
+
         return (
             <>
-            <div className="buttomContainer">
-            <buttom  className="buttom"onClick={()=>{
-                this.run(this.state.grid)
-                this.setState({grid:this.state.auxiliaryGrid})
-                this.setButtom(true)
-                this.setLabel("trocar imagem")
-            }} >{this.state.label}</buttom>
-                {
-                    this.state.buttom?<buttom className="buttom" onClick={()=>{this.deleteStates()
-                    this.setLabel("iniciar jogo")
-                    this.setButtom(false)}}>apagar</buttom>:null
-                }
-            </div>
-            <div className="container">
-        
-            <table className="borderTable" >
-                <tbody >
-                    {     
-                    this.state.grid.map((line, lineIndex) =>
-                        <tr className="borderTable" key={lineIndex} style={{boder:"2px solid black"}}>
-                            {line.map((column,columnIndex)=>{
-                                return(
-                                    <td key={columnIndex} 
-                                    onClick={
-                                        (e)=>{
+                <div className="buttomContainer">
+                    <buttom className="buttom" onClick={() => {
+                        this.run(this.state.grid)
+                        this.setState({ grid: this.state.auxiliaryGrid })
+                        this.setButtom(true)
+                        this.setLabel("trocar imagem")
+                    }} >{this.state.label}</buttom>
+                    {
+                        this.state.buttom ? <buttom className="buttom" onClick={() => {
+                            this.deleteStates()
+                            this.setLabel("iniciar jogo")
+                            this.setButtom(false)
+                        }}>apagar</buttom> : null
+                    }
+                </div>
+                <div className="container">
 
-                                        let copiaState = this.state.grid
-                                        copiaState[lineIndex][columnIndex]=true;
-                                       
-                                        this.setState(copiaState)
-                                        
-                                        }}  style={{height:"20px",width:"20px",border:"2px solid black" ,backgroundColor:this.state.grid[lineIndex][columnIndex]?"red":""}}></td>
-                                )
-                            })}
-                        </tr>
-                    )}
-                </tbody>
-            </table></div></>)
+                    <table className="borderTable" >
+                        <tbody >
+                            {
+                                this.state.grid.map((line, lineIndex) =>
+                                    <tr className="borderTable" key={lineIndex} style={{ boder: "2px solid black" }}>
+                                        {line.map((column, columnIndex) => {
+                                            return (
+                                                <td key={columnIndex}
+                                                    onClick={
+                                                        (e) => {
+
+                                                            let newState = this.state.grid
+                                                            newState[lineIndex][columnIndex] = true;
+
+                                                            this.setState(newState)
+
+                                                        }} style={{ height: "20px", width: "20px", border: "2px solid black", backgroundColor: this.state.grid[lineIndex][columnIndex] ? "red" : "" }}></td>
+                                            )
+                                        })}
+                                    </tr>
+                                )}
+                        </tbody>
+                    </table></div></>)
     }
 }
